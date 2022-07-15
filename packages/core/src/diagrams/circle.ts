6dd3b32af0d7e7a9524bbca87e2b48ee1eb25393
tckxjzc <tckxjzc@gmail.com>
2022-07-15 10:52:07 +0800

    新增扇形

--- packages/core/src/diagrams/circle.ts
+++ packages/core/src/diagrams/circle.ts
@@ -2,17 +2,36 @@ import { Pen } from '../pen';
 
 export function circle(pen: Pen, ctx?: CanvasRenderingContext2D): Path2D {
   const path = !ctx ? new Path2D() : ctx;
+  const { angle } = pen;
   const { x, y, width, height } = pen.calculative.worldRect;
+  let radiusX = width / 2;
+  let radiusY = height / 2;
+  let centerX = x + width / 2;
+  let centerY = y + height / 2;
+  if(!angle){
+    //
+  } else if (angle <= 90) {
+    centerX = x;
+    centerY = y;
+    radiusX=width;
+    radiusY=height;
+  }else if(angle<= 180){
+    centerY= y;
+    radiusY=height;
+  }
   path.ellipse(
-    x + width / 2,
-    y + height / 2,
-    width / 2,
-    height / 2,
+    centerX,
+    centerY,
+    radiusX,
+    radiusY,
     0,
     0,
-    Math.PI * 2
+      angle ? angle / 180 * Math.PI : Math.PI * 2
   );
-
+  if(angle && angle < 360){
+    path.lineTo(centerX, centerY);
+    path.closePath();
+  }
   if (path instanceof Path2D) {
     return path;
   }
